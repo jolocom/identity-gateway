250d5a5c856d6ac1550269c1b5fa99b51851f1de
Panagiotis Kourouklidis <panagiotis.kourouklidis@gmail.com>
2017-08-01 16:12:27 +0200

    sequelize implementation of access rights

--- code/src/access-rights.ts
+++ code/src/access-rights.ts
@@ -3,7 +3,7 @@ import * as moment from 'moment'
 import * as minimatch from 'minimatch'
 
 export interface AccessRights {
-  grant({userID, identity, pattern, read, write, expiryDate, oneTimeToken} : 
+  grant({userID, identity, pattern, read, write, expiryDate, oneTimeToken} :
         {userID : string, identity : string, pattern : string,
          read: boolean, write: false, expiryDate? : moment.Moment, oneTimeToken? : string
         })
@@ -27,7 +27,7 @@ export class MemoryAccessRights implements AccessRights {
     this.rules = {}
   }
 
-  async grant({userID, identity, pattern, read, write, expiryDate, oneTimeToken} : 
+  async grant({userID, identity, pattern, read, write, expiryDate, oneTimeToken} :
               {userID : string, identity : string, pattern : string,
                read: boolean, write: false, expiryDate? : moment.Moment, oneTimeToken? : string
               })
@@ -55,6 +55,49 @@ export class MemoryAccessRights implements AccessRights {
       read: _.some(identityRules, rule => rule.read),
       write: _.some(identityRules, rule => rule.write)
     }
+  }
+
+  _getNow() {
+    return moment()
+  }
+}
+
+export class SequelizeAccessRights implements AccessRights {
+  private _ruleModel
+
+  constructor({ruleModel}) {
+    this._ruleModel = ruleModel
+  }
+
+  async grant({userID, identity, pattern, read, write, expiryDate, oneTimeToken} :
+              {userID : string, identity : string, pattern : string,
+               read: boolean, write: false, expiryDate? : moment.Moment, oneTimeToken? : string
+              })
+  {
+    await this._ruleModel.create({
+      identityId: userID, identity, pattern, read, write, expiryDate, oneTimeToken
+    })
+  }
+
+  async check({userID, identity, path, oneTimeToken} :
+              {userID : string, identity : string, path : string, oneTimeToken? : string}) :
+    Promise<{read : boolean, write : boolean}>
+  {
+    let identityRules = await this._ruleModel.findAll({where: {
+      identityId: userID
+    }})
+    identityRules = _(identityRules)
+      .map((rule, idx) => ({...rule, idx}))
+      .filter(rule => rule.identity === identity)
+      .filter(rule => !rule.token || rule.token === oneTimeToken)
+      .filter(rule => !rule.expiryDate || rule.expiryDate.isAfter(this._getNow()))
+      .filter(rule => minimatch(path, rule.pattern))
+      .valueOf()
+
+      return {
+        read: _.some(identityRules, rule => rule.read),
+        write: _.some(identityRules, rule => rule.write)
+      }
   }
 
   _getNow() {
--- code/src/sequelize/models.ts
+++ code/src/sequelize/models.ts
@@ -20,6 +20,14 @@ export function defineSequelizeModels(sequelize) {
     Verification: {
       identity: Sequelize.STRING,
       signature: Sequelize.TEXT,
+    },
+    Rules: {
+      identity: Sequelize.STRING,
+      pattern : Sequelize.STRING,
+      read: Sequelize.BOOLEAN,
+      write: Sequelize.BOOLEAN,
+      expiryDate: Sequelize.DATE,
+      oneTimeToken: Sequelize.STRING
     }
   }
 
@@ -29,6 +37,7 @@ export function defineSequelizeModels(sequelize) {
 
   models.Attribute.belongsTo(models.Identity)
   models.Verification.belongsTo(models.Attribute)
+  models.Rules.belongsTo(models.Identity)
 
   return models
 }
