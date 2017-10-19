de6707b95da260f2b6d937ea912108bae085d2f8
Vincent den Boer <vincent@shishkabab.net>
2017-10-19 12:19:52 +0200

    Add skeleton Bigchaindb file

---
+++ code/src/bigchaindb-contracts.ts
@@ -0,0 +1,21 @@
+export interface SecurityClaim {
+  
+}
+
+export interface FunctionalityObject {
+
+}
+
+export interface Functionality extends FunctionalityObject {
+
+}
+
+export interface ContractCheckResult {
+
+}
+
+export class BigChainInteractions {
+  constructor({walletManager}) {
+
+  }
+}
