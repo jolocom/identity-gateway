04f73caff5f5bb7c25ef0371002ac12d430fef20
Vincent den Boer <vincent@shishkabab.net>
2017-10-19 12:31:02 +0200

    Further skeleton work

--- code/src/bigchaindb-contracts.ts
+++ code/src/bigchaindb-contracts.ts
@@ -1,3 +1,5 @@
+import { DataSigner } from './data-signer'
+
 export interface SecurityClaim {
   
 }
@@ -17,5 +19,53 @@ export interface ContractCheckResult {
 export class BigChainInteractions {
   constructor({walletManager}) {
 
+  }
+
+  createOwnershipClaim(
+    {seedPhrase, identityURL, contractName} : 
+    {seedPhrase : string, identityURL : string, contractName : string}
+  ) {
+
+  }
+  
+  createFunctionalityObject({
+    seedPhrase, identityURL, contractName,
+    sourceIdentityURL, dataSigner,
+    object
+  } : {
+    seedPhrase : string, identityURL : string, contractName : string,
+    sourceIdentityURL : string, dataSigner : DataSigner,
+    object : FunctionalityObject
+  }) {
+
+  }
+  
+  createFunctionalityClaim({
+    seedPhrase, identityURL, sourceIdentityURL,
+    dataSigner, contractName
+  } : {
+    seedPhrase : string, identityURL : string, sourceIdentityURL : string,
+    dataSigner : DataSigner, contractName : string
+  }) {
+
+  }
+  
+  createSecurityClaim({
+    seedPhrase, identityURL, contractName,
+    sourceIdentityURL, dataSigner,
+    level
+  } : {
+    seedPhrase : string, identityURL : string, contractName : string,
+    sourceIdentityURL : string, dataSigner : DataSigner,
+    level : number
+  }) {
+
+  }
+
+  checkContract(
+    {identityURL, contractName, retrieveHistory} :
+    {identityURL : string, contractName : string, retrieveHistory? : boolean}
+  ) : ContractCheckResult | null {
+    
   }
 }
